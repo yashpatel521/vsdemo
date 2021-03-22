8a205a9b38340f99e4f45e387febff29f0d98527
yash <yash1451999@gmail.com>
2021-03-22 19:24:23 +0530

    run

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajax basic</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+</head>
+
+<body>
+    <!-- <div class="container">
+        <h2 id="changedata">hello world</h2>
+        <button id="btn" class="btn btn-primary">Click Me</button>.
+    </div>
+    <script>
+        $(document).ready(function() {
+            $('#btn').on("click", function() {
+                $("#changedata").load("save.php", function() {
+                    alert('Data changed');
+                })
+            })
+        })
+    </script> -->
+
+    <!-- <div class="container">
+        <div>
+            <h2 id="loaddata">hello world</h2>
+        </div>
+        <button class="btn btn-primary" onclick="myfun()">Check</button>
+    </div>
+    <script>
+        function myfun() {
+            var req = new XMLHttpRequest();
+            req.open('GET', 'save.php', true);
+            req.send();
+
+            req.onreadystatechange = function() {
+                if (req.readyState == 4 && req.status == 200) {
+                    document.getElementById('loaddata').innerHTML = req.responseText;
+                }
+            }
+        }
+    </script> -->
+
+    <div class="container">
+        <div>
+            <h2 id="loaddata">hello world</h2>
+        </div>
+        <button class="btn btn-primary btnclick">Check</button>
+    </div>
+    <script>
+    $(document).ready(function() {
+        $('.btnclick').click(function() {
+
+            // $.get('save.php', function(data, status) {
+            //     $('#loaddata').html(data);
+            //     alert(status);
+            // })
+
+            $.post('save.php', {
+
+                    name: "Yash",
+                    sname: "Patel"
+
+                },
+                function(data, status) {
+
+                    $('#loaddata').html(data);
+                    alert(status);
+
+                })
+        })
+    })
+    </script>
+</body>
+
+</html>
---
+++ save.php
@@ -0,0 +1,8 @@
+<!-- Data changed -->
+
+<?php
+$fname = $_POST['name'];
+$lname = $_POST['sname'];
+
+echo "My name is " . $fname . " " . $lname;
+?>
